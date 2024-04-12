e4918441afde6be38d0de4b9a39cdfe4d5bd866d
KarolWojnar <karolwojnar47@gmail.com>
2024-04-12 13:07:00 +0200

    created post view

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -8,7 +8,9 @@ use Symfony\Component\Routing\Attribute\Route;
 class HelloController extends AbstractController {
 
     private array $messages = [
-        "hello!", "hi :)", "bye :("
+        ['message' => 'hi', 'created' => '2024/03/11'],
+        ['message' => 'hello', 'created' => '2024/02/22'],
+        ['message' => 'bye', 'created' => '2022/05/22']
     ];
 
     #[Route('/{limit<\d+>?3}', name: 'app_index')]
@@ -16,7 +18,8 @@ class HelloController extends AbstractController {
         return $this->render(
             'hello/index.html.twig',
             [
-                'messages' => $this->messages
+                'messages' => $this->messages,
+                'limit' => $limit
             ]
         );
     }
