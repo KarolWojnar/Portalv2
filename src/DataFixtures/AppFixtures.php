adb53a5ba648b60350f25e7a0e8257537a8f1dc5
KarolWojnar <karolwojnar47@gmail.com>
2024-04-20 00:09:48 +0200

    password hasher added

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,14 +3,37 @@
 namespace App\DataFixtures;
 
 use App\Entity\MicroPost;
+use App\Entity\User;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(
+        private UserPasswordHasherInterface $userPasswordHasher
+    ) {
+    }
+
     public function load(ObjectManager $manager): void
     {
+        $user1 = new User();
+        $user1->setEmail("test@wp.pl");
+        $user1->setPassword(
+            $this->userPasswordHasher
+                ->hashPassword($user1, 'passwd123')
+        );
+        $manager->persist($user1);
+
+        $user2 = new User();
+        $user2->setEmail("janek@wp.pl");
+        $user2->setPassword(
+            $this->userPasswordHasher
+                ->hashPassword($user2, 'passwd123')
+        );
+        $manager->persist($user2);
+
         $microPost1 = new MicroPost();
         $microPost1->setTitle('Welcome to poland');
         $microPost1->setText('Its beautyfull country');
